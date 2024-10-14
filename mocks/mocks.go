745e73a110a5ac8365195b86ea1f30059ba23e6d
RickyNJ <rickynijsten@gmail.com>
2024-10-14 17:09:06 +0200

    started treegen again''

--- mocks/mocks.go
+++ mocks/mocks.go
@@ -4,13 +4,42 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 )
+
+type mockDevice struct {
+    commands map[string]*mockNode
+}
+
+type mockNode struct {
+    Value string
+    Output string
+    Next []*mockNode
+}
 
 type mock struct {
     Command string `json:"command"`
     Response string `json:"response"`
-    Arguments string `json:"arguments"`
-    Flags string `json:"flags"`
+}
+
+func updateTree(node *mockNode, command []string) {
+    if len(command) == 1 {
+           //  check if command already has output, if it does not set the output of that node
+        return
+    }
+    updateTree(node, command[1:])
+}
+
+func GenerateMockDevice(mocks []*mock) *mockDevice {
+    device := &mockDevice{}
+    for _, m := range mocks {
+        splitCommands := strings.Split(m.Command, " ")
+        if _, ok := device.commands[splitCommands[0]]; !ok {
+            device.commands[splitCommands[0]] = &mockNode{Value: splitCommands[0]}
+        }
+        updateTree(device.commands[splitCommands[0]], splitCommands)
+    }
+    return device 
 }
 
 func GenerateFromJSON(filepath string) *mock {
@@ -45,3 +74,4 @@ func ReadMappingsDir() []*mock {
 
     return result
 }
+
