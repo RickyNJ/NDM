22c9a1643d5d83d1246ac5e2ac5df89406865385
RickyNJ <rickynijsten@gmail.com>
2024-10-22 18:55:19 +0200

    get final node works, cli input doesnt

--- main.go
+++ main.go
@@ -1,32 +1,33 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-    "strings"
+    // "bufio"
+    "fmt"
+    // "os"
+    //    "strings"
 
-	. "github.com/RickyNJ/NDM/mocks"
+    . "github.com/RickyNJ/NDM/mocks"
 )
 
 func main() {
-    reader := bufio.NewReader(os.Stdin)
+    // reader := bufio.NewReader(os.Stdin)
 
     m := ReadMappingsDir("mappings/")
     d := GenerateMockDevice(m)
-
-
-    for {
-        fmt.Print(">> ")
-        input, _ := reader.ReadString('\n')
-        splitInput := strings.Split(input, " ")
-        fmt.Println(len(splitInput))
-
-        if val, ok := d.Commands[splitInput[0]]; ok {
-            output := GetFinalNode(val, splitInput)
-            fmt.Println(output.Output)
-        } else {
-            fmt.Println("command not configured")
-        }
-    }
+    input := []string{"show", "interface"}
+    final := GetFinalNode(d.Commands["show"], input)
+    fmt.Println(final.Output)
+    // for {
+    //     fmt.Print(">> ")
+    //     input, _ := reader.ReadString('\n')
+    //     splitInput := strings.Split(input, " ")
+    //     fmt.Println(len(splitInput))
+    //
+    //     if val, ok := d.Commands[splitInput[0]]; ok {
+    //         output := GetFinalNode(val, splitInput)
+    //         fmt.Println(output.Output)
+    //     } else {
+    //         fmt.Println("command not configured")
+    //     }
+    // }
 }
